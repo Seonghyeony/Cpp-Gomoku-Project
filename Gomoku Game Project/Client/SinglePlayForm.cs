fa7081156d9a5fdce0f16ae0a5b22b7ee285daac
Seonghyeony <sunghyun7949@naver.com>
2021-05-03 16:27:50 +0900

    C++ 오목 프로젝트

---
+++ Gomoku Game Project/Client/SinglePlayForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class SinglePlayForm : Form
+    {
+        private const int rectSize = 33;    // 오목판의 셀 크기
+        private const int edgeCount = 15;   // 오목판의 선 갯수
+
+        // 15 x 15
+        private enum Horse { none = 0, BLACK, WHITE };
+        private Horse[,] board = new Horse[edgeCount, edgeCount];
+        private Horse nowPlayer = Horse.BLACK;  // 현재 차례의 돌.
+
+        private bool playing = false;   // 진행 중인지 체크
+
+        public SinglePlayForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool judge()    // 승리 판정 함수.
+        {
+            // 가로, 세로, 대각선 체크.
+
+            // 가로
+            for (int i = 0; i < edgeCount - 4; i++)
+                for (int j = 0; j < edgeCount; j++)
+                    if (board[i, j] == nowPlayer && board[i + 1, j] == nowPlayer && board[i + 2, j] == nowPlayer &&
+                        board[i + 3, j] == nowPlayer && board[i + 4, j] == nowPlayer)
+                        return true;
+            // 세로
+            for (int i = 0; i < edgeCount; i++)
+                for (int j = 4; j < edgeCount; j++)
+                    if (board[i, j] == nowPlayer && board[i, j - 1] == nowPlayer && board[i, j - 2] == nowPlayer &&
+                        board[i, j - 3] == nowPlayer && board[i, j - 4] == nowPlayer)
+                        return true;
+            // Y = X 대각선
+            for (int i = 0; i < edgeCount - 4; i++)
+                for (int j = 0; j < edgeCount - 4; j++)
+                    if (board[i, j] == nowPlayer && board[i + 1, j + 1] == nowPlayer && board[i + 2, j + 2] == nowPlayer &&
+                        board[i + 3, j + 3] == nowPlayer && board[i + 4, j + 4] == nowPlayer)
+                        return true;
+            // Y = -X 대각선
+            for (int i = 0; i < edgeCount; i++)
+                for (int j = 0; j < edgeCount - 4; j++)
+                    if (board[i, j] == nowPlayer && board[i - 1, j + 1] == nowPlayer && board[i - 2, j + 2] == nowPlayer &&
+                        board[i - 3, j + 3] == nowPlayer && board[i - 4, j + 4] == nowPlayer)
+                        return true;
+            return false;
+        }
+
+        private void refresh()
+        {
+            // 게임을 초기화.
+            this.boardPicture.Refresh();
+            for (int i = 0; i < edgeCount; i++)
+                for (int j = 0; j < edgeCount; j++)
+                    board[i, j] = Horse.none;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (!playing)
+            {
+                refresh();
+                playing = true;
+                playButton.Text = "재시작";
+                status.Text = nowPlayer.ToString() + " 플레이어의 차례입니다.";
+            }
+            else
+            {
+                refresh();
+                status.Text = "게임이 재시작되었습니다.";
+            }
+        }
+
+        private void boardPicture_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!playing)
+            {
+                MessageBox.Show("게임을 실행해주세요.");
+                return;
+            }
+            Graphics g = this.boardPicture.CreateGraphics();
+            int x = e.X / rectSize;
+            int y = e.Y / rectSize;
+            // 0 ~ 14
+            if (x < 0 || y < 0 || x >= edgeCount || y >= edgeCount)
+            {
+                MessageBox.Show("테두리를 벗어날 수 없습니다.");
+                return;
+            }
+
+            // 해당 위치에 돌을 놓을 수 있으면 놓는다.
+            if (board[x, y] != Horse.none) return;
+            board[x, y] = nowPlayer;
+
+            if (nowPlayer == Horse.BLACK)
+            {
+                SolidBrush brush = new SolidBrush(Color.Black);
+                g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
+            }
+            else
+            {
+                SolidBrush brush = new SolidBrush(Color.White);
+                g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
+            }
+
+            if (judge())
+            {
+                status.Text = nowPlayer.ToString() + " 플레이어가 승리했습니다.";
+                playing = false;
+                playButton.Text = "게임시작";
+            }
+            else
+            {
+                nowPlayer = ((nowPlayer == Horse.BLACK) ? Horse.WHITE : Horse.BLACK);
+                status.Text = nowPlayer.ToString() + " 플레이어의 차례입니다.";
+            }
+        }
+
+        // 오목판 자체를 그리는 함수. Paint 함수는 refresh() 를 했을 때 자동으로 수행됨.
+        private void boardPicture_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gp = e.Graphics;
+            Color lineColor = Color.Black;
+            Pen p = new Pen(lineColor, 2);
+            // 테두리
+            gp.DrawLine(p, rectSize / 2, rectSize / 2, rectSize / 2, rectSize * edgeCount - rectSize / 2);  // 좌측
+            gp.DrawLine(p, rectSize / 2, rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize / 2);  // 상측
+            gp.DrawLine(p, rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2);
+            gp.DrawLine(p, rectSize * edgeCount - rectSize / 2, rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2);
+            p = new Pen(lineColor, 1);
+            // 대각선 방향으로 이동하면서 십자가 모양의 선 그리기
+            for (int i = rectSize + rectSize / 2; i < rectSize * edgeCount - rectSize / 2; i += rectSize)
+            {
+                gp.DrawLine(p, rectSize / 2, i, rectSize * edgeCount - rectSize / 2, i);
+                gp.DrawLine(p, i, rectSize / 2, i, rectSize * edgeCount - rectSize / 2);
+            }
+        }
+    }
+}
